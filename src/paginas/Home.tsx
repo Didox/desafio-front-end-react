7796161459cf124f1b898a485a61fa3601e1fe19
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-08-11 06:58:25 -0300

    Update Home.tsx

--- src/paginas/Home.tsx
+++ src/paginas/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
           <div className="container-fluid">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h6 className="m-0 font-weight-bold text-primary">Home - Continuous Deployment </h6>
+                    <h6 className="m-0 font-weight-bold text-primary">Home - Continuous Deployment - </h6>
                 </div>
                 <div className="card-body">
                     <p>
